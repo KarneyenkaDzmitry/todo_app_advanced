2b275b90367f89e7b4051be7c1164daef2b43987
Dzmitry_Karneyenka <Dzmitry_Karneyenka@epam.com>
2018-06-27 16:56:53 +0300

    create notes class to use moduel like a class

--- notes/notes.js
+++ notes/notes.js
@@ -2,94 +2,91 @@
 const rwdata = require('../util/rwdata.js');
 const dateFormatter = require('../util/dateformatter.js');
 const sorter = require('../util/sorter.js');
-
-function list() {
-    let result = '';
-    const json = rwdata.reader();
-    if (json.notes.length === 0) {
-        result = `The list is empty`;
-    } else {
-        (json.notes).forEach((element, index) => {
-            result += `Note №${index + 1}\n Title: [${element.title}]\n Body : [${element.body}]\n Date :[${element.date}]\n`;
-        });
+class Notes {
+    constructor(){}
+    list() {
+        let result = '';
+        const json = rwdata.reader();
+        if (json.notes.length === 0) {
+            result = `The list is empty`;
+        } else {
+            (json.notes).forEach((element, index) => {
+                result += `Note №${index + 1}\n Title: [${element.title}]\n Body : [${element.body}]\n Date :[${element.date}]\n`;
+            });
+        }
+        return result;
     }
-    return result;
-}
 
-function read(title) {
-    let result = `There isn\'t any notes with title: ${title}.`;
-    const json = rwdata.reader()
-    if (json.notes.length === 0) {
-        result = `The list is empty.`
-    } else {
+    read(title) {
+        let result = `There isn\'t any notes with title: ${title}.`;
+        const json = rwdata.reader()
+        if (json.notes.length === 0) {
+            result = `The list is empty.`
+        } else {
+            (json.notes).forEach((element, index) => {
+                if (element.title === title) {
+                    result = ` Title: [${element.title}]\n Body : [${element.body}]`;
+                }
+            });
+        }
+        return result;
+    }
+
+    add(newTitle, newBody, marker) {
+        let json = rwdata.reader();
+        for (let i = 0; i < json.notes.length; i++) {
+            if (json.notes[i].title === newTitle) {
+                if (!marker) {
+                    return `The note with title: [${newTitle}] has already existed in the list.`;
+                } else {
+                    this.remove(newTitle);
+                }
+            }
+        };
+        json = rwdata.reader();
+        const data = {
+            title: newTitle,
+            body: newBody,
+            date: dateFormatter.getStringNewDate()
+        };
+        json.notes.push(data);
+        const ind = rwdata.writer(JSON.stringify(json));
+        if (ind === true) {
+            return `New note with title: [${newTitle}] and body: [${newBody}] was successfully added or updated to list.`;
+        } else {
+            return ind;
+        }
+    }
+
+    remove(title) {
+        let json = rwdata.reader();
+        let result = `There isn\'t a note with title ${title}`;
         (json.notes).forEach((element, index) => {
             if (element.title === title) {
-                result = ` Title: [${element.title}]\n Body : [${element.body}]`;
+                json.notes.splice(index, 1);
+                rwdata.writer(JSON.stringify(json));
+                result = `The note \n Title: ${element.title}\n Body :${element.body}\n was successfully deleted.`;
             }
         });
+        return result;
     }
-    return result;
-}
 
-function add(newTitle, newBody, marker) {
-    let json = rwdata.reader();
-    for (let i = 0; i < json.notes.length; i++) {
-        if (json.notes[i].title === newTitle) {
-            if (!marker) {
-                return `The note with title: [${newTitle}] has already existed in the list.`;
-            } else {
-                remove(newTitle);
-            }
+    sort(kind, options) {
+        let json = rwdata.reader();
+        let result = '';
+        if (json.notes.length === 0) {
+            result = `The list is empty.`
+        } else {
+            result = '{"notes":' + JSON.stringify(sorter.sort(json.notes, kind, options)) + '}';
+            rwdata.writer(result);
         }
-    };
-    json = rwdata.reader();
-    const data = {
-        title: newTitle,
-        body: newBody,
-        date: dateFormatter.getStringNewDate()
-    };
-    json.notes.push(data);
-    const ind = rwdata.writer(JSON.stringify(json));
-    if (ind === true) {
-        return `New note with title: [${newTitle}] and body: [${newBody}] was successfully added or updated to list.`;
-    } else {
-        return ind;
+        return this.list();
+    }
+
+    clear() {
+        rwdata.writer('{"notes":[]}');
+        return `The list is empty.`;
     }
 }
+module.exports = new Notes();
 
-function remove(title) {
-    let json = rwdata.reader();
-    let result = `There isn\'t a note with title ${title}`;
-    (json.notes).forEach((element, index) => {
-        if (element.title === title) {
-            json.notes.splice(index, 1);
-            rwdata.writer(JSON.stringify(json));
-            result = `The note \n Title: ${element.title}\n Body :${element.body}\n was successfully deleted.`;
-        }
-    });
-    return result;
-}
-
-function sort(kind, options) {
-    let json = rwdata.reader();
-    let result = '';
-    if (json.notes.length === 0) {
-        result = `The list is empty.`
-    } else {
-        result = '{"notes":' + JSON.stringify(sorter.sort(json.notes, kind, options)) + '}';
-        rwdata.writer(result);
-    }
-    return list();
-}
-
-function clear() {
-    rwdata.writer('{"notes":[]}');
-    return `The list is empty.`;
-}
-
-exports.clear = clear;
-exports.remove = remove;
-exports.add = add;
-module.exports.read = read;
-exports.list = list;
-exports.sort = sort;
--- util/dateformatter.js
+++ util/dateformatter.js
@@ -1,9 +1,5 @@
 'use strict';
 
-let currentDate = new Date();
-//const stringD = getStringDate(currentDate)
-//console.log(stringD);
-//const convertDate = getInstanceDate(stringD);
 function getStringNewDate() { 
     const date = new Date();
     return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
@@ -15,13 +11,7 @@ function getInstanceDate(stringDate) {
     let mass = stringDate.split(/\s/);
     let mass1 = mass[0].split('/');
     let result = `${mass1[1]}/${mass1[0]}/${mass1[2]} ${mass[1]}`;
-    //console.log(result);
     return new Date(result);
 }
-//console.log(convertDate.valueOf());
-
 exports.getStringNewDate = getStringNewDate;
 exports.getInstanceDate = getInstanceDate;
-
-
-//exports = {getStringDate: "getStringDate", getInstanceDate:"getInstanceDate", getStringNewDate:"getStringNewDate"};
